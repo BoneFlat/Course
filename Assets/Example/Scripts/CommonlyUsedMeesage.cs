ec3e5c155c2e546b47f551b33da85f98560ce02f
thangnh <thangnh.sas@gmail.com>
2023-08-25 07:38:44 +0700

    example

---
+++ Assets/Example/Scripts/CommonlyUsedMeesage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Example
+{
+    public class CommonlyUsedMeesage : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+
+        private void OnValidate()
+        {
+            if (_rigidbody2D == null)
+                _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, 5f);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, Vector3.one);
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            Debug.Log("on focus");
+        }
+
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            Debug.Log("on pause");
+        }
+    }
+}
---
+++ Assets/Example/Scripts/CommonlyUsedMethod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Example
+{
+    public class CommonlyUsedMethod : MonoBehaviour
+    {
+        private void Start()
+        {
+            Count();
+            CoCount();
+        }
+
+        public void Count()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Debug.Log($"{Time.frameCount} count = {i}");
+            }
+        }
+
+        public void CoCount()
+        {
+            StartCoroutine(IECount());
+            
+            IEnumerator IECount()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    yield return null;
+                    // yield return new WaitForEndOfFrame();
+                    Debug.Log($"Co {Time.frameCount} count = {i}");
+                }
+            }
+        }
+    }
+}
