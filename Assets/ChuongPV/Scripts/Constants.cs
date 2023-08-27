74b0745b7affec08a59c1ddba42f0944b528d2c6
chuong_gamer <hieutg244@gmail.com>
2023-08-27 13:53:13 +0700

    Revert "homework1"
    This reverts commit c2049d4e8085673bada57cc7bf0d24cd56a58ca7.

--- Assets/ChuongPV/Scripts/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChuongPV
-{
-    public static class Constants
-    {
-        public static readonly string ENEMY = "Enemy";
-        public static readonly string PLAYER = "Player";
-    }
-}
