f54baa0a233449447b5fd239a62cf2f4861401c8
thangnh <thangnh.sas@gmail.com>
2023-09-11 06:39:38 +0700

    physic ex

---
+++ Assets/Example/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Example.Scripts
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float _smooth = 0.2f;
+
+        private Vector3 _offset;
+
+        private void Start()
+        {
+            _offset = target.position - transform.position;
+            gameObject.layer = LayerMask.NameToLayer("Default");
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = Vector3.Lerp(transform.position,target.position - _offset, _smooth);
+        }
+
+        
+    }
+}
---
+++ Assets/Example/Scripts/ExRigidbody.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+namespace Example.Scripts
+{
+    public class ExRigidbody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        [SerializeField] private MovingMode _movingMode;
+        [SerializeField] private float speed = 5;
+
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log($"On trigger {other.name}");
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log($"On Collision {other.gameObject.name}");
+        }
+
+        private void FixedUpdate()
+        {
+            if (_rigidbody2D != null)
+            {
+                if (Input.GetAxisRaw("Horizontal") == 0)
+                    return;
+                
+                switch (_movingMode)
+                {
+                    case MovingMode.Velocity:
+                        _rigidbody2D.velocity =
+                            Input.GetAxisRaw("Horizontal") * speed * Vector2.right ;
+                        
+                        Debug.Log(_rigidbody2D.velocity);
+                        break;
+
+                    case MovingMode.MovePosition:
+                        _rigidbody2D.MovePosition(_rigidbody2D.position +
+                                                  Input.GetAxisRaw("Horizontal") * speed * Time.fixedDeltaTime *
+                                                  Vector2.right);
+
+                        break;
+                    case MovingMode.Transform:
+                        _rigidbody2D.position += Input.GetAxisRaw("Horizontal") * speed * Time.fixedDeltaTime *
+                                                 Vector2.right;
+                        break;
+                }
+            }
+        }
+
+        public enum MovingMode
+        {
+            Velocity,
+            MovePosition,
+            Transform
+        }
+    }
+}
