098a24a19a599cc36ba9cc13aafc8012782bb7dd
PhuocNG0308 <71546982+PhuocNG0308@users.noreply.github.com>
2023-09-10 22:42:35 +0700

    Finish HW2

--- Assets/Example/Scripts/LoadScene/FakeLoader.cs
+++ Assets/Example/Scripts/LoadScene/FakeLoader.cs
@@ -5,14 +5,19 @@
 	using UnityEngine;
 	using UnityEngine.SceneManagement;
 	using UnityEngine.UI;
+	using System.Threading.Tasks;
+	using System.Net.Http;
+	using System;
 
 	public class FakeLoader : MonoBehaviour
 	{
 		[SerializeField] private Image _loadingFill;
 		[SerializeField] private Text  textPercent;
 		[SerializeField] private float _loadTime = 1f;
+		[SerializeField] private Text downloadingText;
 
 		private int  percent = 0;
+		private bool stopLoading = false;
 		private void Start()
 		{
 			// GameEventHandler.OnLoadGame
@@ -37,8 +42,17 @@
 			float t = 0;
 			while (t < _loadTime)
 			{
-				percent =  Mathf.Clamp((int)(t / _loadTime * 100), 0, 95);
-				t       += Time.fixedDeltaTime;
+				percent = Mathf.Clamp((int)(t / _loadTime * 100), 0, 95);
+				t += Time.fixedDeltaTime;
+
+
+
+				if ((int)percent >= 60)
+				{
+					downloadingText.gameObject.SetActive(true);
+					FetchData();
+				}
+
 
 				_loadingFill.fillAmount = t / _loadTime;
 
@@ -47,6 +61,30 @@
 
 			yield return new WaitForSeconds(delaySeconds);
 			sceneAsync.allowSceneActivation = true;
+		}
+
+		private async Task FetchData()
+        {
+			await DownloadData();
+		}			
+
+		private async Task DownloadData()
+		{
+			string url = "https://dotnetfoundation.org";
+
+			using (HttpClient httpClient = new HttpClient())
+			{
+				try
+				{
+					string result = await httpClient.GetStringAsync(url);
+					downloadingText.text = "Download Complete!!!";
+					Debug.Log(result);
+				}
+				catch (HttpRequestException e)
+				{
+					Debug.Log($"Error: {e.Message}");
+				}
+			}
 		}
 	}
 }
